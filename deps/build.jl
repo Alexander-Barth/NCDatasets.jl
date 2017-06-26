bcaeddbf6298c5c02abd0d3bafd8b44cb1cceadc
Alexander Barth <barth.alexander@gmail.com>
2017-06-26 21:01:27 +0200

    update dependencies

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,11 @@
 using BinDeps
-#using Conda
+using Conda
 
 @BinDeps.setup
 libnetcdf = library_dependency("libnetcdf", aliases = ["libnetcdf4","libnetcdf-7","netcdf"])
 
 #Conda.add_channel("conda-forge")
-#provides(Conda.Manager, "libnetcdf", libnetcdf)
+provides(Conda.Manager, "libnetcdf", libnetcdf)
 provides(AptGet, "libnetcdf-dev", libnetcdf, os = :Linux)
 provides(Yum, "netcdf-devel", libnetcdf, os = :Linux)
 
