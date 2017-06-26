e25339c59f3b64159d5aeb95684debb139c6afe8
Alexander Barth <barth.alexander@gmail.com>
2017-06-26 22:42:28 +0200

    check version number in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,16 @@
 using BinDeps
 using Conda
 
+function validate_netcdf_version(name,handle)
+    f = Libdl.dlsym_e(handle, "nc_inq_libvers")
+    verstr = unsafe_string(ccall(f,Ptr{UInt8},()))
+
+    ver = VersionNumber(split(verstr)[1])
+    return ver > v"4.2"
+end
+
 @BinDeps.setup
-libnetcdf = library_dependency("libnetcdf", aliases = ["libnetcdf4","libnetcdf-7","netcdf"])
+libnetcdf = library_dependency("libnetcdf", aliases = ["libnetcdf4","libnetcdf-7","netcdf"], validate = validate_netcdf_version)
 
 #Conda.add_channel("conda-forge")
 provides(Conda.Manager, "libnetcdf", libnetcdf)
