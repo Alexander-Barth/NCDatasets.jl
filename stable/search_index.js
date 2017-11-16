7cef6236a6f41d721223b936bf9b2777a983399b
autodocs <autodocs>
2017-11-16 22:06:15 +0000

    build based on a5e4553

--- stable/search_index.js
+++ stable/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.Dataset",
     "category": "Type",
-    "text": "Dataset(filename::AbstractString,mode::AbstractString = \"r\";\n                 format::Symbol = :netcdf4)\n\nCreate a new NetCDF file if the mode is \"c\". An existing file with the same  name will be overwritten. If mode is \"a\", then an existing file is open into  append mode (i.e. existing data in the NetCDF file is not overwritten and  a variabale can be added). With the mode equal to \"r\", an existing NetCDF file or OPeNDAP URL can be open in read-only mode.  The default mode is \"r\".\n\nSupported formats:\n\n:netcdf4 (default): HDF5-based NetCDF format\n:netcdf4_classic: Only netCDF 3 compatible API features will be used\n:netcdf3_classic: classic NetCDF format supporting only files smaller than 2GB.\n:netcdf3_64bit_offset: improved NetCDF format supporting files larger than 2GB.\n\nFiles can also be open and automatically closed with a do block.\n\nDataset(\"file.nc\") do ds\n    data = ds[\"temperature\"][:,:]\nend \n\n\n\n"
+    "text": "Dataset(filename::AbstractString,mode::AbstractString = \"r\";\n                 format::Symbol = :netcdf4)\n\nCreate a new NetCDF file if the mode is \"c\". An existing file with the same name will be overwritten. If mode is \"a\", then an existing file is open into append mode (i.e. existing data in the NetCDF file is not overwritten and a variabale can be added). With the mode equal to \"r\", an existing NetCDF file or OPeNDAP URL can be open in read-only mode.  The default mode is \"r\".\n\nSupported formats:\n\n:netcdf4 (default): HDF5-based NetCDF format\n:netcdf4_classic: Only netCDF 3 compatible API features will be used\n:netcdf3_classic: classic NetCDF format supporting only files smaller than 2GB.\n:netcdf3_64bit_offset: improved NetCDF format supporting files larger than 2GB.\n\nFiles can also be open and automatically closed with a do block.\n\nDataset(\"file.nc\") do ds\n    data = ds[\"temperature\"][:,:]\nend\n\n\n\n"
 },
 
 {
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Base.keys",
     "category": "Function",
-    "text": "keys(a::Attributes)\n\nReturn a list of the names of all attributes.\n\n\n\nkeys(ds::Dataset)\n\nReturn a list of all variables names in Dataset ds. \n\n\n\n"
+    "text": "keys(a::Attributes)\n\nReturn a list of the names of all attributes.\n\n\n\nkeys(ds::Dataset)\n\nReturn a list of all variables names in Dataset ds.\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Base.haskey",
     "category": "Function",
-    "text": "haskey(ds::Dataset,varname)\n\nReturn true of the Dataset ds has a variable with the name varname. For example:\n\nds = Dataset(\"/tmp/test.nc\",\"r\")\nif haskey(ds,\"temperature\")\n    println(\"The file has a variable 'temperature'\")\nend\n\nThis example checks if the file /tmp/test.nc has a variable with the  name temperature.\n\n\n\n"
+    "text": "haskey(ds::Dataset,varname)\n\nReturn true of the Dataset ds has a variable with the name varname. For example:\n\nds = Dataset(\"/tmp/test.nc\",\"r\")\nif haskey(ds,\"temperature\")\n    println(\"The file has a variable 'temperature'\")\nend\n\nThis example checks if the file /tmp/test.nc has a variable with the name temperature.\n\n\n\n"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.variable",
     "category": "Function",
-    "text": "variable(ds::Dataset,varname::String)\n\nReturn the NetCDF variable varname in the dataset ds as a  NCDataset.Variable. No scaling is applied when this variable is  indexes.\n\n\n\n"
+    "text": "variable(ds::Dataset,varname::String)\n\nReturn the NetCDF variable varname in the dataset ds as a NCDataset.Variable. No scaling is applied when this variable is indexes.\n\n\n\n"
 },
 
 {
@@ -61,7 +61,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Base.close",
     "category": "Function",
-    "text": "close(ds::Dataset)\n\nClose the Dataset ds. All pending changes will be written  to the disk.\n\n\n\n"
+    "text": "close(ds::Dataset)\n\nClose the Dataset ds. All pending changes will be written to the disk.\n\n\n\n"
 },
 
 {
@@ -77,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.defVar",
     "category": "Function",
-    "text": "defVar(ds::Dataset,name,vtype,dimnames; kwargs...)\n\nDefine a variable with the name name in the dataset ds.  vtype can be Julia types in the table below (with the corresponding NetCDF type).  The parameter dimnames is a tuple with the names of the dimension.  For scalar this parameter is the empty tuple ().   The variable is returned (of the type CFVariable).\n\nKeyword arguments\n\nfillvalue: A value filled in the NetCDF file to indicate missing data.   It will be stored in the _FillValue attribute.\nchunksizes: Vector integers setting the chunk size. The total size of a chunk must be less than 4 GiB. \ndeflatelevel: Compression level: 0 (default) means no compression and 9 means maximum compression. Each chunk will be compressed individually.\nshuffle: If true, the shuffle filter is activated which can improve the compression ratio.\nchecksum: The checksum method can be :fletcher32 or :nochecksum (checksumming is disabled, which is the default)\n\nchunksizes, deflatelevel, shuffle and checksum can only be  set on NetCDF 4 files.\n\nNetCDF data types\n\nNetCDF Type Julia Type\nNC_BYTE Int8\nNC_UBYTE UInt8\nNC_SHORT Int16\nNC_INT Int32\nNC_INT64 Int64\nNC_FLOAT Float32\nNC_DOUBLE Float64\nNC_CHAR Char\nNC_STRING String\n\n\n\n"
+    "text": "defVar(ds::Dataset,name,vtype,dimnames; kwargs...)\n\nDefine a variable with the name name in the dataset ds.  vtype can be Julia types in the table below (with the corresponding NetCDF type).  The parameter dimnames is a tuple with the names of the dimension.  For scalar this parameter is the empty tuple (). The variable is returned (of the type CFVariable).\n\nKeyword arguments\n\nfillvalue: A value filled in the NetCDF file to indicate missing data.  It will be stored in the _FillValue attribute.\nchunksizes: Vector integers setting the chunk size. The total size of a chunk must be less than 4 GiB.\ndeflatelevel: Compression level: 0 (default) means no compression and 9 means maximum compression. Each chunk will be compressed individually.\nshuffle: If true, the shuffle filter is activated which can improve the compression ratio.\nchecksum: The checksum method can be :fletcher32 or :nochecksum (checksumming is disabled, which is the default)\n\nchunksizes, deflatelevel, shuffle and checksum can only be set on NetCDF 4 files.\n\nNetCDF data types\n\nNetCDF Type Julia Type\nNC_BYTE Int8\nNC_UBYTE UInt8\nNC_SHORT Int16\nNC_INT Int32\nNC_INT64 Int64\nNC_FLOAT Float32\nNC_DOUBLE Float64\nNC_CHAR Char\nNC_STRING String\n\n\n\n"
 },
 
 {
@@ -101,7 +101,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.chunking",
     "category": "Function",
-    "text": "storage,chunksizes = chunking(v::Variable)\n\nReturn the storage type (:contiguous or :chunked) and the chunk sizes  of the varable v.\n\n\n\n"
+    "text": "storage,chunksizes = chunking(v::Variable)\n\nReturn the storage type (:contiguous or :chunked) and the chunk sizes of the varable v.\n\n\n\n"
 },
 
 {
@@ -109,7 +109,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.deflate",
     "category": "Function",
-    "text": "shuffle,deflate,deflate_level = deflate(v::Variable)\n\nReturn compression information of the variable v. If shuffle  is true, then shuffling (byte interlacing) is activaded. If  deflate is true, then the data chunks (see chunking) are  compressed using the compression level deflate_level  (0 means no compression and 9 means maximum compression).\n\n\n\n"
+    "text": "shuffle,deflate,deflate_level = deflate(v::Variable)\n\nReturn compression information of the variable v. If shuffle is true, then shuffling (byte interlacing) is activaded. If deflate is true, then the data chunks (see chunking) are compressed using the compression level deflate_level (0 means no compression and 9 means maximum compression).\n\n\n\n"
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.checksum",
     "category": "Function",
-    "text": "checksummethod = checksum(v::Variable)\n\nReturn the checksum method of the variable v which can be either  be :fletcher32 or :nochecksum.\n\n\n\n"
+    "text": "checksummethod = checksum(v::Variable)\n\nReturn the checksum method of the variable v which can be either be :fletcher32 or :nochecksum.\n\n\n\n"
 },
 
 {
@@ -133,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Variables",
     "category": "section",
-    "text": "defVar\ndimnames\nname\nchunking\ndeflate\nchecksum\nBase.start(ds::Dataset)"
+    "text": "defVar\ndimnames\nname\nchunking\ndeflate\nchecksum\nBase.start(ds::Dataset)Different type of arrays are involved when working with NCDatasets. For instance assume that test.nc is a file with a Float32 variable called var. Assume that we open this data set in append mode (\"a\"):using NCDatasets\nds = Dataset(\"test.nc\",\"a\")\nv_cf = ds[\"var\"]The variable v_cf has the type CFVariable. No data is actually loaded from disk, but you can query its size, number of dimensions, number elements, ... by the functions size, ndims, length as ordinary Julia arrays. Once you index, the variable v_cf, then the data is loaded and stored into a DataArray:v_da = v_cf[:,:]"
 },
 
 {
@@ -141,7 +141,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Base.getindex",
     "category": "Method",
-    "text": "getindex(a::Attributes,name::AbstractString)\n\nReturn the value of the attribute called name from the  attribute list a. Generally the attributes are loaded by  indexing, for example:\n\nds = Dataset(\"file.nc\")\ntitle = ds.attrib[\"title\"]\n\n\n\n"
+    "text": "getindex(a::Attributes,name::AbstractString)\n\nReturn the value of the attribute called name from the attribute list a. Generally the attributes are loaded by indexing, for example:\n\nds = Dataset(\"file.nc\")\ntitle = ds.attrib[\"title\"]\n\n\n\n"
 },
 
 {
@@ -149,7 +149,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Base.setindex!",
     "category": "Method",
-    "text": "Base.setindex!(a::Attributes,data,name::AbstractString)\n\nSet the attribute called name to the value data in the  attribute list a. Generally the attributes are defined by  indexing, for example:\n\nds = Dataset(\"file.nc\",\"c\")\nds.attrib[\"title\"] = \"my title\"\n\n\n\n"
+    "text": "Base.setindex!(a::Attributes,data,name::AbstractString)\n\nSet the attribute called name to the value data in the attribute list a. Generally the attributes are defined by indexing, for example:\n\nds = Dataset(\"file.nc\",\"c\")\nds.attrib[\"title\"] = \"my title\"\n\n\n\n"
 },
 
 {
@@ -173,7 +173,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "NCDatasets.defDim",
     "category": "Function",
-    "text": "defDim(ds::Dataset,name,len)\n\nDefine a dimension in the data-set ds with the given name and length len. If len is the special value Inf, then the dimension is considered as  unlimited, i.e. it will grow as data is added to the NetCDF file.\n\nFor example:\n\nds = Dataset(\"/tmp/test.nc\",\"c\")\ndefDim(ds,\"lon\",100)\n\nThis defines the dimension lon with the size 100.\n\n\n\n"
+    "text": "defDim(ds::Dataset,name,len)\n\nDefine a dimension in the data-set ds with the given name and length len. If len is the special value Inf, then the dimension is considered as unlimited, i.e. it will grow as data is added to the NetCDF file.\n\nFor example:\n\nds = Dataset(\"/tmp/test.nc\",\"c\")\ndefDim(ds,\"lon\",100)\n\nThis defines the dimension lon with the size 100.\n\n\n\n"
 },
 
 {
@@ -181,7 +181,7 @@ var documenterSearchIndex = {"docs": [
     "page": "NCDatasets.jl",
     "title": "Base.setindex!",
     "category": "Method",
-    "text": "Base.setindex!(d::Dimensions,len,name::AbstractString)\n\nDefines the dimension called name to the length len.  Generally dimension are defined by indexing, for example:\n\nds = Dataset(\"file.nc\",\"c\")\nds.dim[\"longitude\"] = 100\n\nIf len is the special value Inf, then the dimension is considered as  unlimited, i.e. it will grow as data is added to the NetCDF file.\n\n\n\n"
+    "text": "Base.setindex!(d::Dimensions,len,name::AbstractString)\n\nDefines the dimension called name to the length len. Generally dimension are defined by indexing, for example:\n\nds = Dataset(\"file.nc\",\"c\")\nds.dim[\"longitude\"] = 100\n\nIf len is the special value Inf, then the dimension is considered as unlimited, i.e. it will grow as data is added to the NetCDF file.\n\n\n\n"
 },
 
 {
@@ -190,6 +190,22 @@ var documenterSearchIndex = {"docs": [
     "title": "Dimensions",
     "category": "section",
     "text": "defDim\nsetindex!(d::NCDatasets.Dimensions,len,name::AbstractString)"
+},
+
+{
+    "location": "index.html#NCDatasets.ncgen-Tuple{Any}",
+    "page": "NCDatasets.jl",
+    "title": "NCDatasets.ncgen",
+    "category": "Method",
+    "text": "ncgen(fname; ...)\nncgen(fname,jlname; ...)\n\nGenerate the Julia code that would produce a NetCDF file with the same metadata as the NetCDF file fname. The code is placed in the file jlname or printed to the standard output. Per default the new NetCDF file is called filename.nc. This can be changed with the optional parameter newfname.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Utility-functions-1",
+    "page": "NCDatasets.jl",
+    "title": "Utility functions",
+    "category": "section",
+    "text": "ncgen(fname)"
 },
 
 {
