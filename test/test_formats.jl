3bbc6d1ab9a77f12d95c734b209ce8bfce1c3362
Alexander Barth <barth.alexander@gmail.com>
2018-05-16 22:45:59 +0200

    fix some 0.7 depreciations (8)

--- test/test_formats.jl
+++ test/test_formats.jl
@@ -3,23 +3,23 @@ sz = (4,5)
 # The mode "c" stands for creating a new file (clobber)
 
 for format in [:netcdf4, :netcdf4_classic, :netcdf3_classic, :netcdf3_64bit_offset]
-    filename = tempname()
-    NCDatasets.Dataset(filename,"c"; format = format) do ds
+    filenamefmt = tempname()
+    NCDatasets.Dataset(filenamefmt,"c"; format = format) do ds
 
         # define the dimension "lon" and "lat"
         NCDatasets.defDim(ds,"lon",sz[1])
         NCDatasets.defDim(ds,"lat",sz[2])
         
         # variables
-        v = NCDatasets.defVar(ds,"var-Float32",Float32,("lon","lat"))
+        vfmt = NCDatasets.defVar(ds,"var-Float32",Float32,("lon","lat"))
         
         # write array
-        v[:,:] = fill(Float32(123),size(v))
+        vfmt[:,:] = fill(Float32(123),size(vfmt))
         
         # check content
-        @test all(v[:,:][:] .== 123)
+        @test all(vfmt[:,:][:] .== 123)
     end
-    rm(filename)
+    rm(filenamefmt)
 end
 
 @test_throws ErrorException NCDatasets.Dataset(tempname(),"c";
