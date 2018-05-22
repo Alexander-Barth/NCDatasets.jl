5ad7bf0fca40ae37a1864955b9b193d5eca4e87e
ctroupin <charles.troupin@gmail.com>
2018-05-22 15:47:59 +0200

    corrected variable names

--- test/test_varbyatt.jl
+++ test/test_varbyatt.jl
@@ -14,8 +14,8 @@ defDim(ds,"lat",nlat)
 lon = defVar(ds,"lon",Float32,("lon",))
 lat= defVar(ds,"lat",Float32,("lat",))
 # create two variables with the same standard name
-vort = defVar(ds,"vorticity",Float32,("lon","lat"))
-vort2 = defVar(ds,"vorticity2",Float32,("lon","lat"))
+vort = defVar(ds,"vort",Float32,("lon","lat"))
+vort2 = defVar(ds,"vort2",Float32,("lon","lat"))
 
 # write attributes
 lon.attrib["standard_name"] = "longitude"
