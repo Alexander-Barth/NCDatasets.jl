9e21cf544973552ebd5f2610b762805385567103
Alexander Barth <barth.alexander@gmail.com>
2018-09-05 21:41:29 +0200

    fix test

--- test/test_formats.jl
+++ test/test_formats.jl
@@ -9,20 +9,19 @@ for format in [:netcdf4, :netcdf4_classic, :netcdf3_classic, :netcdf3_64bit_offs
         # define the dimension "lon" and "lat"
         NCDatasets.defDim(ds,"lon",sz[1])
         NCDatasets.defDim(ds,"lat",sz[2])
-        
+
         # variables
         vfmt = NCDatasets.defVar(ds,"var-Float32",Float32,("lon","lat"))
-        
+
         # write array
         vfmt[:,:] = fill(Float32(123),size(vfmt))
-        
+
         # check content
         @test all(vfmt[:,:][:] .== 123)
     end
     rm(filenamefmt)
 end
 
-@test_throws ErrorException NCDatasets.Dataset(tempname(),"c";
-                                      format = :netcdf3000_perfect_format)
-
-
+@test_throws NCDatasets.NetCDFError NCDatasets.Dataset(
+    tempname(),"c";
+    format = :netcdf3000_perfect_format)
