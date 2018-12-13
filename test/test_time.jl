01c48f5c6f0e0af77cedcd58d41df2bf4312036e
Alexander Barth <barth.alexander@gmail.com>
2018-12-13 11:44:21 +0100

    more tests for date ranges

--- test/test_time.jl
+++ test/test_time.jl
@@ -366,11 +366,13 @@ end
 
 @test length(DateTimeNoLeap(2000, 01, 01):Day(1):DateTimeNoLeap(2000, 12, 31)) == 365
 @test length(DateTimeNoLeap(2000, 01, 01):Month(1):DateTimeNoLeap(2000, 12, 31)) == 12
-@test length(DateTimeNoLeap(2000, 01, 01):Year(1):DateTimeNoLeap(2001, 1, 1)) == 2
 
 for T in [DateTimeStandard, DateTimeJulian, DateTimeProlepticGregorian,
           DateTimeAllLeap, DateTimeNoLeap, DateTime360Day]
 
+    # end date is inclusive
+    @test length(T(2000, 01, 01):Month(1):T(2001, 1, 1)) == 13
+    @test length(T(2000, 01, 01):Year(1):T(2001, 1, 1)) == 2
 end
 
 # if @isdefined DataArrays
