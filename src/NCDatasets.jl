baeb3c4dfadba2d1c97f8ffe2f722a4241f802e0
Alexander Barth <barth.alexander@gmail.com>
2019-10-29 14:56:24 +0100

    update doc

--- src/NCDatasets.jl
+++ src/NCDatasets.jl
@@ -1,3 +1,20 @@
+"""
+NCDatasets is a module to read and write NetCDF files.
+This is minimal example how to read a NetCDF file.
+
+```julia
+# open the file and show its metadata (if called in the REPL without ending semicolon)
+ds = Dataset("filename.nc","r")
+# load all data of the variable temperature
+v = ds["temperature"][:,:]
+# load the attribute units
+unit = v.attrib["units"]
+# close the file
+close(ds)
+```
+
+More information is available at https://github.com/Alexander-Barth/NCDatasets.jl .
+"""
 module NCDatasets
 using Dates
 using Printf
