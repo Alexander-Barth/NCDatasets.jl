b71aa66b8bf245910cea1267f3f785a8fca2a2ff
Alexander Barth <barth.alexander@gmail.com>
2020-02-11 12:03:55 +0100

    restructure files (5)

---
+++ src/attributes.jl
@@ -0,0 +1,85 @@
+# `Attributes` is a collection of named attributes
+
+
+"Return all attribute names"
+function listAtt(ncid,varid)
+    natts = nc_inq_varnatts(ncid,varid)
+    names = Vector{String}(undef,natts)
+
+    for attnum = 0:natts-1
+        names[attnum+1] = nc_inq_attname(ncid,varid,attnum)
+    end
+
+    return names
+end
+
+
+function Base.get(a::BaseAttributes, name::AbstractString,default)
+    if haskey(a,name)
+        return a[name]
+    else
+        return default
+    end
+end
+
+
+"""
+    getindex(a::Attributes,name::AbstractString)
+
+Return the value of the attribute called `name` from the
+attribute list `a`. Generally the attributes are loaded by
+indexing, for example:
+
+```julia
+ds = NCDataset("file.nc")
+title = ds.attrib["title"]
+```
+"""
+function Base.getindex(a::Attributes,name::AbstractString)
+    return nc_get_att(a.ncid,a.varid,name)
+end
+
+
+"""
+    Base.setindex!(a::Attributes,data,name::AbstractString)
+
+Set the attribute called `name` to the value `data` in the
+attribute list `a`. Generally the attributes are defined by
+indexing, for example:
+
+```julia
+ds = NCDataset("file.nc","c")
+ds.attrib["title"] = "my title"
+```
+"""
+function Base.setindex!(a::Attributes,data,name::AbstractString)
+    defmode(a.ncid,a.isdefmode) # make sure that the file is in define mode
+    return nc_put_att(a.ncid,a.varid,name,data)
+end
+
+"""
+    Base.keys(a::Attributes)
+
+Return a list of the names of all attributes.
+"""
+Base.keys(a::Attributes) = listAtt(a.ncid,a.varid)
+
+
+function Base.show(io::IO, a::BaseAttributes; indent = "  ")
+    try
+        # use the same order of attributes than in the NetCDF file
+        for (attname,attval) in a
+            print(io,indent,@sprintf("%-20s = ",attname))
+            printstyled(io, @sprintf("%s",attval),color=:blue)
+            print(io,"\n")
+        end
+    catch err
+        if isa(err,NetCDFError)
+            if err.code == NC_EBADID
+                print(io,"NetCDF attributes (file closed)")
+                return
+            end
+        end
+        rethrow
+    end
+end
---
+++ src/dimensions.jl
@@ -0,0 +1,87 @@
+# `Dimensions` is a collection of named dimensions
+# each dimension has a name and a size (possibly unlimited)
+
+"""
+    keys(d::Dimensions)
+
+Return a list of all dimension names in NCDataset `ds`.
+
+# Examples
+
+```julia-repl
+julia> ds = NCDataset("results.nc", "r");
+julia> dimnames = keys(ds.dim)
+```
+"""
+function Base.keys(d::Dimensions)
+    return String[nc_inq_dimname(d.ncid,dimid)
+                  for dimid in nc_inq_dimids(d.ncid,false)]
+end
+
+function Base.getindex(a::Dimensions,name::AbstractString)
+    dimid = nc_inq_dimid(a.ncid,name)
+    return nc_inq_dimlen(a.ncid,dimid)
+end
+
+"""
+    unlimited(d::Dimensions)
+
+Return the names of all unlimited dimensions.
+"""
+function unlimited(d::Dimensions)
+    return String[nc_inq_dimname(d.ncid,dimid)
+                  for dimid in nc_inq_unlimdims(d.ncid)]
+end
+
+export unlimited
+
+"""
+    Base.setindex!(d::Dimensions,len,name::AbstractString)
+
+Defines the dimension called `name` to the length `len`.
+Generally dimension are defined by indexing, for example:
+
+```julia
+ds = NCDataset("file.nc","c")
+ds.dim["longitude"] = 100
+```
+
+If `len` is the special value `Inf`, then the dimension is considered as
+`unlimited`, i.e. it will grow as data is added to the NetCDF file.
+"""
+function Base.setindex!(d::Dimensions,len,name::AbstractString)
+    defmode(d.ncid,d.isdefmode) # make sure that the file is in define mode
+    dimid = nc_def_dim(d.ncid,name,(isinf(len) ? NC_UNLIMITED : len))
+    return len
+end
+
+"""
+    defDim(ds::NCDataset,name,len)
+
+Define a dimension in the data set `ds` with the given `name` and length `len`.
+If `len` is the special value `Inf`, then the dimension is considered as
+`unlimited`, i.e. it will grow as data is added to the NetCDF file.
+
+For example:
+
+```julia
+ds = NCDataset("/tmp/test.nc","c")
+defDim(ds,"lon",100)
+```
+
+This defines the dimension `lon` with the size 100.
+"""
+function defDim(ds::NCDataset,name,len)
+    defmode(ds.ncid,ds.isdefmode) # make sure that the file is in define mode
+    dimid = nc_def_dim(ds.ncid,name,(isinf(len) ? NC_UNLIMITED : len))
+    return nothing
+end
+export defDim
+
+function renameDim(ds::NCDataset,oldname,newname)
+    defmode(ds.ncid,ds.isdefmode) # make sure that the file is in define mode
+    dimid = nc_inq_dimid(ds.ncid,oldname)
+    nc_rename_dim(ds.ncid,dimid,newname)
+    return nothing
+end
+export renameDim
