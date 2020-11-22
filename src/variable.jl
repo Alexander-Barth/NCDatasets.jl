956cc1020cf85f2b59f16911de6159f388394e83
Martijn Visser <mgvisser@gmail.com>
2020-11-22 21:43:05 +0100

    docstring typo

--- src/variable.jl
+++ src/variable.jl
@@ -211,7 +211,7 @@ export chunking
     isshuffled,isdeflated,deflate_level = deflate(v::Variable)
 
 Return compression information of the variable `v`. If shuffle
-is `true`, then shuffling (byte interlacing) is activaded. If
+is `true`, then shuffling (byte interlacing) is activated. If
 deflate is `true`, then the data chunks (see `chunking`) are
 compressed using the compression level `deflate_level`
 (0 means no compression and 9 means maximum compression).
