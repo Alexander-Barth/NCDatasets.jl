6e2fd8ff40fcc73e2a366de634a3d0285d705a6b
Alexander Barth <barth.alexander@gmail.com>
2021-05-19 10:55:32 +0200

    prevent showing a variable from a closed file

--- src/variable.jl
+++ src/variable.jl
@@ -464,6 +464,8 @@ function Base.setindex!(v::Variable,data,indexes::Union{Int,Colon,UnitRange{Int}
     return v[ind...] = data
 end
 
+# necessary for IJulia if showing a variable from a closed file
+Base.show(io::IO,::MIME"text/plain",v::Union{Variable,CFVariable}) = show(io,v)
 
 function Base.show(io::IO,v::AbstractVariable; indent="")
     delim = " × "
