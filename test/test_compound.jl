7d3b78e869072c089cfa832cd4b96d490ef61705
Alexander Barth <barth.alexander@gmail.com>
2022-02-09 22:36:49 +0100

    more generic test

--- test/test_compound.jl
+++ test/test_compound.jl
@@ -5,41 +5,47 @@ using NCDatasets: nc_create, NC_NETCDF4, NC_CLOBBER, nc_def_dim, nc_def_compound
 # mutable struct are not supported
 # https://discourse.julialang.org/t/passing-an-array-of-structures-through-ccall/5194
 
-struct s5
+struct s8
    i1::Cint
    i2::Cint
+   f1::Cfloat
+   d1::Cdouble
 end
 
 sz = (2,3)
 
-compound_data = Array{s5,2}(undef,sz)
+data = Array{s8,2}(undef,sz)
 
 
 for j = 1:sz[2]
     for i = 1:sz[1]
-        compound_data[i,j] = s5(i,j)
+        data[i,j] = s8(i,j,1.2,2.3)
     end
 end
 
 
-ncid = nc_create(filename, NC_NETCDF4|NC_CLOBBER)
+T = eltype(data)
 
+ncid = nc_create(filename, NC_NETCDF4|NC_CLOBBER)
 
 x_dimid = nc_def_dim(ncid, "x", sz[1])
 y_dimid = nc_def_dim(ncid, "y", sz[2])
 
 dimids = [x_dimid, y_dimid]
 
-@assert ncType[Cint] == NC_INT
+typeid = nc_def_compound(ncid, sizeof(T), "sample_compound_type")
 
-typeid = nc_def_compound(ncid, sizeof(s5), "sample_compound_type")
-
-nc_insert_compound(ncid, typeid, "i1", fieldoffset(s5,1), NC_INT)
-nc_insert_compound(ncid, typeid, "i2", fieldoffset(s5,2), NC_INT)
+for i = 1:fieldcount(T)
+    nctype = ncType[fieldtype(T,i)]
+    nc_insert_compound(
+        ncid, typeid,
+        fieldname(T,i),
+        fieldoffset(T,i), nctype)
+end
 
 varid = nc_def_var(ncid, "data", typeid, reverse(dimids))
 
-nc_put_var(ncid, varid, compound_data)
+nc_put_var(ncid, varid, data)
 nc_close(ncid)
 
 
