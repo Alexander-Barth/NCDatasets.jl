b88ebedbc7d8d766f2c12fca54e37fd89560ba4c
Alexander Barth <barth.alexander@gmail.com>
2022-02-17 11:15:10 +0100

    example with variable with an unlimited dimensions

--- src/dimensions.jl
+++ src/dimensions.jl
@@ -82,11 +82,31 @@ If `len` is the special value `Inf`, then the dimension is considered as
 For example:
 
 ```julia
+using NCDatasets
 ds = NCDataset("/tmp/test.nc","c")
 defDim(ds,"lon",100)
+# [...]
+close(ds)
 ```
 
 This defines the dimension `lon` with the size 100.
+
+To create a variable with an unlimited dimensions use for example:
+
+```julia
+using NCDatasets
+ds = NCDataset("/tmp/test.nc","c")
+defDim(ds,"lon",10)
+defDim(ds,"lat",10)
+defDim(ds,"time",Inf)
+defVar(ds,"unlimited_variable",Float64,("lon","lat","time"))
+@show ds.dim["time"]
+# returns 0 as no data is added
+ds["unlimited_variable"][:,:,:] = randn(10,10,4)
+@show ds.dim["time"]
+# returns now 4 as 4 time slice have been added
+close(ds)
+```
 """
 function defDim(ds::NCDataset,name,len)
     defmode(ds) # make sure that the file is in define mode
