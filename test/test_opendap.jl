1348c043caeae7cccdf282986ce5c9b0b30b6bfd
Alexander Barth <barth.alexander@gmail.com>
2022-06-07 15:38:27 +0200

    try to set ENV["TEMP"]

--- test/test_opendap.jl
+++ test/test_opendap.jl
@@ -1,6 +1,10 @@
 using NCDatasets
 using Test
 
+
+@show get(ENV,"TEMP","not defined")
+
+ENV["TEMP"] = "."
 ds = NCDataset("https://erddap.ifremer.fr/erddap/griddap/SDC_GLO_CLIM_TS_V2_1")
 
 time = ds["time"][:]
