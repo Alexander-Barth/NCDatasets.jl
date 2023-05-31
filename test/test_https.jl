9c72c38199ddd058f0a416060192af966595d150
Alexander Barth <barth.alexander@gmail.com>
2023-05-31 10:24:16 +0200

    fix name conflict

--- test/test_https.jl
+++ test/test_https.jl
@@ -1,10 +1,10 @@
 using NCDatasets
 using Test
 
-url = "https://rda.ucar.edu/thredds/dodsC/files/g/ds084.1/2018/20181231/gfs.0p25.2018123118.f003.grib2"
+sample_url = "https://rda.ucar.edu/thredds/dodsC/files/g/ds084.1/2018/20181231/gfs.0p25.2018123118.f003.grib2"
 
 @test begin
-    NCDataset(url) do ds
+    NCDataset(sample_url) do ds
         haskey(ds,"lon")
     end
 end
