94c6310f1a1f052725264626db90adc50d848ea2
Tristan Carion <tristancarion@gmail.com>
2023-06-07 12:22:20 +0200

    remove comments

--- src/variable.jl
+++ src/variable.jl
@@ -333,10 +333,6 @@ end
 
 _read_data_from_nc!(v::Variable, aout) = _read_data_from_nc!(v, aout, 1)
 
-# NetCDF scalars indexed as []
-# readblock!(v::Variable{T, 0}, aout) where T = readblock!(v, aout, 1)
-# writeblock!(v::Variable{T, 0}, data) where T = writeblock!(v, data, 1)
-
 function writeblock!(v::Variable, data, indexes::TI...) where TI <: Union{AbstractUnitRange,StepRange}
     datamode(v.ds)
     _write_data_to_nc(v, data, indexes...)
