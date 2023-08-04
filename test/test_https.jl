b14cfbbd170990a40f11f2ae747052477f199663
Alexander Barth <barth.alexander@gmail.com>
2023-08-04 10:22:56 +0200

    disable opendap test (server unreliable)

--- test/test_https.jl
+++ test/test_https.jl
@@ -3,8 +3,9 @@ using Test
 
 sample_url = "https://rda.ucar.edu/thredds/dodsC/files/g/ds084.1/2018/20181231/gfs.0p25.2018123118.f003.grib2"
 
-@test begin
-    NCDataset(sample_url) do ds
-        haskey(ds,"lon")
-    end
-end
+# opendal server too unreliable
+# @test begin
+#     NCDataset(sample_url) do ds
+#         haskey(ds,"lon")
+#     end
+# end
