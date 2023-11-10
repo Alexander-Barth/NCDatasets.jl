5376e45df366a91dc26e6e7e097717db97ea00f1
Alexander Barth <barth.alexander@gmail.com>
2023-11-10 11:39:05 +0100

    more precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,21 +1,41 @@
 @compile_workload begin
-    println("Precompile NCDatasets");
+    println("Precompile NCDatasets 4");
 
     fname = tempname()
     ds = NCDataset(fname,"c");
-    lon = -180:180
-    lat = -90:90
-    time2 = DateTime(2000,1,1):Day(1):DateTime(2000,1,3)
-    SST = randn(length(lon),length(lat),length(time2))
+    sz = (2,2)
 
-    defVar(ds,"lon",lon,("lon",));
-    defVar(ds,"lat",lat,("lat",));
-    defVar(ds,"time",time2,("time",));
-    defVar(ds,"SST",SST,("lon","lat","time"));
+    ds.dim["lon"] = sz[1]
+    ds.dim["lat"] = sz[2]
+
+    for T in [UInt8,Int8,UInt16,Int16,
+              UInt32,Int32,UInt64,Int64,
+              Float32,Float64,
+              Char,String]
+        local data, scalar_data
+        data, scalar_data =
+            if T == String
+                [Char(i+60) * Char(j+60) for i = 1:sz[1], j = 1:sz[2]], "abcde"
+            else
+                [T(i+2*j) for i = 1:sz[1], j = 1:sz[2]], T(100)
+            end
+
+        v = NCDatasets.defVar(ds,"var-$T",T,("lon","lat"))
+        v[:,:] = data
+        v[1,1] = scalar_data
+        data2 = data[:,:]
+        scalar_data2 = data[1,1]
+
+        v.attrib["attrib-$T"] = scalar_data
+        ds.attrib["attrib-$T"] = scalar_data
+
+        scalar_data2 = v.attrib["attrib-$T"]
+        scalar_data2 = ds.attrib["attrib-$T"]
+    end
 
     io = IOBuffer();
     show(io,ds);
-    sum(ds["SST"][:,:,:])
     close(ds);
+
 end
 
