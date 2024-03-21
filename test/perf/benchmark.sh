27a29922b282e097cb230d8e4b5b7eeb8c6cdb7f
Alexander Barth <barth.alexander@gmail.com>
2024-03-21 16:31:00 +0100

    make the caches droping optional

--- test/perf/benchmark.sh
+++ test/perf/benchmark.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-julia   benchmark-julia-NCDatasets.jl
-python3 benchmark-python-netCDF4.py
-Rscript benchmark-R-ncdf4.R
+args="$@"
+julia   benchmark-julia-NCDatasets.jl $args
+python3 benchmark-python-netCDF4.py $args
+Rscript benchmark-R-ncdf4.R $args
 
 julia summary.jl
